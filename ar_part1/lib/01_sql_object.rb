ed5eba39ce6c1b3f9ef531f7ea3fa22fe90d8623
ACotant <austin.cotant@gmail.com>
2018-09-07 14:15:40 -0700

    Finish #initialize for SQLObject

--- ar_part1/lib/01_sql_object.rb
+++ ar_part1/lib/01_sql_object.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require_relative 'db_connection'
 require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
@@ -5,18 +6,30 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    unless @columns
+      results = DBConnection.execute2(<<-SQL)
+        SELECT *
+        FROM #{table_name}
+      SQL
+      @columns = results.first.map {|result| result.to_sym}
+    end 
+    @columns
   end
 
   def self.finalize!
+    columns.each do |attr_name|
+      define_method(attr_name) {attributes[attr_name]}
+      define_method("#{attr_name}=") {|value| attributes[attr_name] = value}
+    end 
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    class_name_str = self.to_s.downcase + "s"
+    @table_name ||= class_name_str
   end
 
   def self.all
@@ -32,11 +45,15 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      raise "unknown attribute '#{attr_name}'" unless self.class.columns.include?(attr_name)
+      self.send("#{attr_name}=", value)
+    end 
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
