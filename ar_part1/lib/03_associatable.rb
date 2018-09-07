d166e05c971bcbba61669fdf24604c8ca78684fd
ACotant <austin.cotant@gmail.com>
2018-09-07 16:55:52 -0700

    Complete #initialize for HasMany and BelongsTo

--- ar_part1/lib/03_associatable.rb
+++ ar_part1/lib/03_associatable.rb
@@ -19,21 +19,42 @@ class AssocOptions
 end
 
 class BelongsToOptions < AssocOptions
+  
   def initialize(name, options = {})
-    # ...
-  end
+    defaults = {
+      foreign_key: "#{name}_id".to_sym,
+      primary_key: :id,
+      class_name: name.to_s.camelcase
+  }
+    defaults.merge!(options)
+    # byebug
+    @primary_key = defaults[:primary_key]
+    @foreign_key = defaults[:foreign_key]
+    @class_name = defaults[:class_name]
+  end 
 end
 
 class HasManyOptions < AssocOptions
+  
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{self_class_name.downcase}_id".to_sym,
+      primary_key: :id,
+      class_name: name.to_s.camelcase.singularize
+  }
+  # byebug
+  defaults.merge!(options)
+  @primary_key = defaults[:primary_key]
+  @foreign_key = defaults[:foreign_key]
+  @class_name = defaults[:class_name]  
   end
+  
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    
   end
 
   def has_many(name, options = {})
