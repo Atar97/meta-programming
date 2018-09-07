173bac8f3c7df86a70ec2750a671570a5239743e
ACotant <austin.cotant@gmail.com>
2018-09-07 16:31:48 -0700

    Add searchable module to be able to use sql where

--- ar_part1/lib/02_searchable.rb
+++ ar_part1/lib/02_searchable.rb
@@ -2,11 +2,32 @@ require_relative 'db_connection'
 require_relative '01_sql_object'
 
 module Searchable
+  
   def where(params)
-    # ...
+    question_str = ""
+    values = []
+    params.each do |key, value|
+      question_str += "#{key} = ? AND "
+      values << value
+    end 
+    
+    question_str = question_str[0...-4]
+    # byebug
+    results = DBConnection.execute(<<-SQL, *values)
+      SELECT 
+        *
+      FROM 
+        #{self.table_name}
+      WHERE
+        #{question_str}
+    SQL
+    self.parse_all(results)
   end
+  
 end
 
 class SQLObject
-  # Mixin Searchable here...
+
+  extend Searchable
+  
 end
